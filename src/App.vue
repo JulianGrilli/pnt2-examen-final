b8975614e3fd354b3093a21c7b8bb945a1ad7c79
juliangrilli <julianm.grilli@gmail.com>
2020-12-01 20:20:07 -0300

    subiendo la resolucion del examen final de NT2

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <EjercicioFinal />
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Numero de pregunta</th>
+      <th scope="col">Respuesta</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>B</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>C</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>B</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>D</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>D</td>
+    </tr>
+  </tbody>
+</table>
+  </div> 
+</template>
+
+<script>
+import EjercicioFinal from '../src/components/EjercicioFinal.vue'
+export default {
+  name: 'App',
+  components: {
+    EjercicioFinal
+  }
+}
+</script>
+
+<style>
+  #app {
+  }
+
+  table{
+    margin-top: 50px;
+  }
+</style>
---
+++ src/components/EjercicioFinal.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <section class="src-components-ejercicio-final">
+    Color: <input type="text" v-model="colorTexto" style="margin:10px;" >
+    Fondo: <input type="text" v-model="fondoTexto" style="margin:10px;" >
+    Size: <input type="text" v-model="sizeTexto" style="margin:10px;" >
+    <br>
+    <hr>
+    Texto: <input type="text" 
+    :style="{ color: colorTexto, backgroundColor: fondoTexto, fontSize: sizeTexto }"
+    v-model="mensaje" style="margin:10px;" >
+    <br>
+    <br>
+    <span v-if="contarPalabras > 0"><strong>Cantidad de palabras:</strong> {{contarPalabras}}</span>
+  </section>
+
+</template>
+
+<script>
+  export default  {
+    name: 'src-components-ejercicio-final',
+    props: [],
+    mounted () {
+      
+    },
+    data () {
+      return {
+        colorTexto : '',
+        fondoTexto : '',
+        sizeTexto : '',
+        mensaje: ''
+      }
+    },
+    methods: {
+      
+    },
+    computed: {
+      contarPalabras(){
+        const arrayMessage = this.mensaje.split(' ');
+        let arrayFinal = []
+        arrayMessage.forEach(palabra => {
+          if(palabra != ""){
+            arrayFinal.push(palabra)
+          }
+        })
+        return arrayFinal.length;
+      },
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-ejercicio-final {
+    margin-left: 20px;
+  }
+  .mensajeSpan{
+    margin-left: 10px;
+  }
+</style>
